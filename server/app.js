aee16c50a01ed578e11f7833123118762f333dbe
Brian Park <park-brian@users.noreply.github.com>
2022-06-28 23:37:45 -0400

    use middleware

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ import fs from "fs";
 import { getLogger } from "./services/logger.js";
 import { apiRouter } from "./services/api.js";
 import { validateEnvironment } from "./services/environment.js";
+import { logErrors, logRequests } from "./services/middleware.js";
 const { APP_NAME, API_PORT, LOG_LEVEL, INPUT_FOLDER, OUTPUT_FOLDER } = process.env;
 
 // ensure that all environment variables are set
@@ -24,7 +25,9 @@ export function createApp() {
 
   // register services as app locals
   app.locals.logger = getLogger(APP_NAME, LOG_LEVEL);
+  app.use(logRequests());
   app.use("/api", apiRouter);
+  app.use(logErrors);
 
   for (const folder of [INPUT_FOLDER, OUTPUT_FOLDER]) fs.mkdirSync(folder, { recursive: true });
 
