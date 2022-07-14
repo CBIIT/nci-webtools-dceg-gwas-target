fe351fae4db6c1fadb9047227f92bbfeadaddf98
Brian Park <park-brian@users.noreply.github.com>
2022-07-14 14:49:37 -0400

    add database snippets

--- server/services/api.js
+++ server/services/api.js
@@ -6,8 +6,8 @@ import multer from "multer";
 import { runMagma } from "./analysis.js";
 import { withAsync } from "./middleware.js";
 import AWS from "aws-sdk";
+import { createSqliteTableFromFile, getSqliteConnection } from "./database.js";
 const { INPUT_FOLDER, OUTPUT_FOLDER, DATA_BUCKET } = process.env;
-
 
 export const apiRouter = Router();
 
@@ -22,15 +22,17 @@ const storage = multer.diskStorage({
   },
   filename: function (req, file, cb) {
     const { logger } = req.app.locals;
-    logger.info(req.body.request_id)
+    logger.info(req.body.request_id);
 
     logger.debug(`Original filename: ${file.originalname}`);
-    if (file.fieldname === 'geneAnalysisBim' || file.fieldname === 'geneAnalysisBed' || file.fieldname === 'geneAnalysisFam') {
-      logger.debug(`New Filename ${req.body.request_id.concat(path.extname(file.originalname))}`)
-      cb(null, req.body.request_id.concat(path.extname(file.originalname)))
-    }
-    else
-      cb(null, file.originalname);
+    if (
+      file.fieldname === "geneAnalysisBim" ||
+      file.fieldname === "geneAnalysisBed" ||
+      file.fieldname === "geneAnalysisFam"
+    ) {
+      logger.debug(`New Filename ${req.body.request_id.concat(path.extname(file.originalname))}`);
+      cb(null, req.body.request_id.concat(path.extname(file.originalname)));
+    } else cb(null, file.originalname);
   },
 });
 
@@ -59,6 +61,13 @@ apiRouter.post(
 
     logger.info(request.body);
     await runMagma(body, logger);
+    // generate database
+    // const resultsFolder = path.resolve(OUTPUT_FOLDER, request_id);
+    // const resultsFile = path.resolve(resultsFolder, "gene_analysis.genes.out");
+    // const databasePath = path.resolve(resultsFolder, "results.db");
+    // const connection = getSqliteConnection(databasePath);
+    // await createSqliteTableFromFile(connection, "gene", resultsFile, {delimiter: "\t"});
+    // upload results to s3 bucket
     logger.info(`[${request.body.request_id}] Finish /submit`);
     response.json("Finished Magma");
   })
@@ -80,24 +89,43 @@ apiRouter.post(
 );
 
 apiRouter.post(
+  "/query-results",
+  withAsync(async (req, res) => {
+    const { request_id, table, columns, conditions, orderBy, offset, limit } = request.body;
+    const databasePath = path.resolve(OUTPUT_FOLDER, request_id, "results.db");
+    // ensure database file exists (eg: downloaded from s3 bucket) before proceeding
+    // ensureLocalFileExists(s3ResultsPath, databasePath); // TODO: implement
+    const connection = getSqliteConnection(databasePath);
+    const results = await connection
+      .select(columns || "*")
+      .from(table)
+      .where(conditions)
+      .orderBy(orderBy)
+      .offset(offset || 0)
+      .limit(limit || 100000);
+    res.json(results);
+  })
+);
+
+apiRouter.post(
   "/fetch-results",
   withAsync(async (request, response) => {
-
     const { logger } = request.app.locals;
     logger.info(request.body);
 
     if (!request.submitted) {
       logger.info(`Execute /fetch-results sample file`);
       const s3 = new AWS.S3();
-      const filestream = await s3.getObject({
-        Bucket: DATA_BUCKET,
-        Key: `gwastarget/gene_analysis.genes.out`
-      }).createReadStream();
+      const filestream = await s3
+        .getObject({
+          Bucket: DATA_BUCKET,
+          Key: `gwastarget/gene_analysis.genes.out`,
+        })
+        .createReadStream();
 
-      filestream.pipe(response)
+      filestream.pipe(response);
       logger.info(`Finish /fetch-results sample file`);
-    } 
-    else {
+    } else {
       const { request_id } = request.body;
       logger.info(`[${request_id}] Execute /fetch-results`);
       const resultsFolder = path.resolve(OUTPUT_FOLDER, request_id);
