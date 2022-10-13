f1289cc828148d795d313fb07e5a37dd878aa961
Brian Park <park-brian@users.noreply.github.com>
2022-10-13 11:18:09 -0400

    use logger in worker

--- server/worker.js
+++ server/worker.js
@@ -1,11 +1,9 @@
 import path from "path";
 import { isMainModule, readJson } from "./services/utils.js";
+import { createLogger } from "./services/logger.js";
 import { runMagma } from "./services/magma.js";
 
-console.log("loaded worker");
-
 if (isMainModule(import.meta)) {
-  console.log("in main module");
   try {
     await main(process.argv, process.env);
     process.exit(0);
@@ -20,6 +18,7 @@ export async function main(argv = process.argv, env = process.env) {
   if (!id) throw new Error("Missing id");
   const paramsFilePath = path.resolve(env.INPUT_FOLDER, id, "params.json");
   const params = await readJson(paramsFilePath);
-  console.log({ params });
-  console.log(await runMagma(params, console));
+  const logger = createLogger(env.APP_NAME, env.LOG_LEVEL);
+  logger.log({ params });
+  return await runMagma(params, console);
 }
