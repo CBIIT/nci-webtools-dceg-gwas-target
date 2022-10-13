6d37fed437f36545e65bc29b31a14b27c79a1e20
Brian Park <park-brian@users.noreply.github.com>
2022-10-13 01:43:37 -0400

    update router

--- client/src/app.js
+++ client/src/app.js
@@ -1,75 +1,16 @@
-import { Suspense, lazy } from "react";
-import { RecoilRoot } from "recoil";
-import { HashRouter as Router, Routes, Route, NavLink } from "react-router-dom";
-import Navbar from "react-bootstrap/Navbar";
-import Nav from "react-bootstrap/Nav";
-import Container from "react-bootstrap/Container";
-import Loader from "./modules/common/loader";
-import ErrorBoundary from "./modules/common/error-boundary";
-import "./styles/main.scss";
-
-// preload lazy-loaded page components
-const Home = preloadLazyComponent(() => import("./modules/home/home"));
-const Analysis = preloadLazyComponent(() => import("./modules/analysis/analysis"));
-const About = preloadLazyComponent(() => import("./modules/about/about"));
-
-function preloadLazyComponent(factory) {
-  const loader = factory();
-  return lazy(() => loader);
-}
+import { Outlet } from "react-router-dom";
+import Navbar from "./modules/navbar/navbar";
+import { routes } from "./routes";
 
 export default function App() {
-  const links = [
-    {
-      route: "/home",
-      title: "Home",
-      component: Home,
-    },
-    {
-      route: "/",
-      title: "Analysis",
-      component: Analysis,
-    },
-    {
-      route: "/about",
-      title: "About",
-      component: About,
-    },
-  ];
+  const childRoutes = routes[0].children.filter((route) => !route.hide);
 
   return (
-    <RecoilRoot>
-      <Router>
-        <div className="d-flex flex-column flex-grow-1">
-          <Navbar expand="sm" variant="dark" className="py-0 flex-none-auto bg-dark">
-            <Container className="px-5">
-              <Navbar.Toggle aria-controls="app-navbar" />
-              <Navbar.Collapse id="app-navbar">
-                <Nav className="d-flex w-100">
-                  {links.map((link, index) => (
-                    <NavLink key={`navlink-${index}`} to={link.route} className="nav-link">
-                      {link.title}
-                    </NavLink>
-                  ))}
-                </Nav>
-              </Navbar.Collapse>
-            </Container>
-          </Navbar>
-          <div id="main-content" className="d-flex flex-column flex-grow-1">
-            <ErrorBoundary fallback="">
-              <Suspense fallback={<Loader>Loading Page</Loader>}>
-                <Routes>
-                  <Route key={`route-${1}`} path={"/:id"} element={<Analysis />} />
-                  {links.map((link, index) => (
-                    <Route key={`route-${index}`} path={link.route} element={<link.component />} />
-                  ))}
-                </Routes>
-              </Suspense>
-            </ErrorBoundary>
-          </div>
-        </div>
-      </Router>
-      {/* <pre>{JSON.stringify(process.env, null, 2)}</pre> */}
-    </RecoilRoot>
+    <>
+      <Navbar routes={childRoutes} />
+      <div className="bg-light">
+        <Outlet />
+      </div>
+    </>
   );
 }
