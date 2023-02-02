8aeee23685d74e732633a30dc9a75f40479df8a2
bchen49 <benchen4998@gmail.com>
2023-02-02 10:50:58 -0500

    update shell file names and paths

--- server/bin/run.dhs.filter.on.magma.file.sh
+++ server/bin/run.dhs.filter.on.magma.file.sh
@@ -2,14 +2,14 @@
 
 tail -n+2 $1| awk '{print "chr"$1"\t"$3-1"\t"$3"\t"$0}' > $1.bed
 
-bedtools intersect -a $1.bed -b ~/Downloads/roadmap.dhs.files/$2 > $1.filtered.by.$2.bed
+bedtools intersect -a $1.bed -b ~/Downloads/roadmap.dhs.files/$2 > $1.filtered.bed
 
-head -1 $1 > $1.filtered.by.$2.for.magma.tsv 
+head -1 $1 > filtered.for.magma.tsv 
 
-awk '{print $4"\t"$5"\t"$6"\t"$7}' $1.filtered.by.$2.bed >> $1.filtered.by.$2.for.magma.tsv
+awk '{print $4"\t"$5"\t"$6"\t"$7}' $1.filtered.bed >> filtered.for.magma.tsv
 
 
 mkdir $1.intermediate.files
 
 mv $1.bed $1.intermediate.files
-mv $1.filtered.by.$2.bed $1.intermediate.files
+mv filtered.for.magma.tsv $1.intermediate.files
