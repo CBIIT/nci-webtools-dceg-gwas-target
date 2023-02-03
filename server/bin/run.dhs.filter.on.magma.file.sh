81f0facb367a9eaeff0cf611f61711518a4122da
bchen49 <benchen4998@gmail.com>
2023-02-03 13:31:18 -0500

    use filtered bed file for calculation

--- server/bin/run.dhs.filter.on.magma.file.sh
+++ server/bin/run.dhs.filter.on.magma.file.sh
@@ -2,12 +2,12 @@
 
 tail -n+2 $1| awk '{print "chr"$1"\t"$3-1"\t"$3"\t"$0}' > $1.bed
 
-bedtools intersect -a $1.bed -b $2 > $1.filtered.bed
+bedtools intersect -a $1.bed -b $2 > results.filtered.bed
 
 head -1 $1 > filtered.for.magma.tsv 
 
-awk '{print $4"\t"$5"\t"$6"\t"$7}' $1.filtered.bed >> filtered.for.magma.tsv
+awk '{print $4"\t"$5"\t"$6"\t"$7}' results.filtered.bed >> filtered.for.magma.tsv
 
-mv $1.filtered.bed $3
+mv results.filtered.bed $3
 mv $1.bed $3
 mv filtered.for.magma.tsv $3
